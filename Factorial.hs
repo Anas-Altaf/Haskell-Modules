fa5226b8a49a09787978a7202856e0aae96924c5
Anas_Altaf <anasaltaf35@gmail.com>
2024-03-24 14:37:28 +0500

    Added Module of Factorial

---
+++ Factorial.hs
@@ -0,0 +1,8 @@
+--Normal Way
+factorial :: (Num a, Enum a) => a -> a
+factorial n = product [1..n]
+
+--Recursive Way
+factorial2 :: (Ord t, Num t) => t -> t
+factorial2 n |n<1 = 1
+factorial2 n = n*factorial2 (n-1)
