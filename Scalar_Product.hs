6af4129e951f63474c4a857b11a90e0c0291dc70
Anas Altaf <157444488+Anas-Altaf@users.noreply.github.com>
2024-04-06 23:39:46 +0500

    Update Scalar_Product.hs

--- Scalar_Product.hs
+++ Scalar_Product.hs
@@ -1,4 +1,13 @@
 scalarProduct :: Num a => [a] -> [a] -> a
+-- Base Cases
 scalarProduct _ [] = 0
 scalarProduct [] _ = 0
+-- Actual 
 scalarProduct xs ys = sum [x * y | (x,y)<-zip xs ys]
+main :: IO ()
+main = do
+
+  let l1 = [1,2,3]
+  let l2 = [4,5,6]
+  let result = scalarProduct l1 l2
+  print result -- 32
